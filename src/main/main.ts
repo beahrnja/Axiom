e2a534cc0056fbf6bb6055a807e436295b05a2f8
Bea Hrnja <beahrnja@gmail.com>
2025-11-21 17:31:16 -0500

    Initial Axiom setup: Electron + React + TypeScript working

---
+++ src/main/main.ts
@@ -0,0 +1,56 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+    // Create the browser window
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        minWidth: 800,
+        minHeight: 600,
+        titleBarStyle: 'hiddenInset',
+        trafficLightPosition: { x: 10, y: 10 },
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false,
+        },
+    });
+
+    // Load the index.html
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.loadURL('http://localhost:9000');
+        mainWindow.webContents.openDevTools();
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+    }
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// IPC handlers
+ipcMain.handle('ping', async () => {
+    return 'pong';
+});
---
+++ src/main/preload.ts
@@ -0,0 +1,8 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+    // We'll add API methods here later
+    ping: () => ipcRenderer.invoke('ping'),
+});
